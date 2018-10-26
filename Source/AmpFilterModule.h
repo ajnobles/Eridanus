3d3be380ce7ffd4f75902c4f1d47014083ce4e5f
Allen Nobles <allennobles@mac.com>
2018-10-26 09:42:31 -0500

    Refactored Amp/Env Filter Modules

--- Source/AmpFilterModule.h
+++ Source/AmpFilterModule.h
@@ -16,76 +16,12 @@ class AmpFilterModule : public CustomComponent
 {
 public:
 	AmpFilterModule ( Slider* as, Slider* ds, Slider* ss, Slider* rs,
-		              Label*  al, Label*  dl, Label*  sl, Label*  rl)
-		            : AttackSlider (as), DecaySlider (ds), 
-		              SustainSlider (ss), ReleaseSlider (rs),
-		              AttackSliderLabel (al), DecaySliderLabel (dl),
-		              SustainSliderLabel (sl), ReleaseSliderLabel (rl)
-	{
-		addAndMakeVisible ( AttackSlider );
-		AttackSlider->setRange ( 0, 100 );
-		AttackSlider->setSliderStyle ( Slider::LinearVertical );
-		AttackSlider->setTextBoxStyle ( Slider::TextBoxBelow, true, 50, 20 );
+		              Label*  al, Label*  dl, Label*  sl, Label*  rl);
 
-		addAndMakeVisible ( DecaySlider );
-		DecaySlider->setRange ( 0, 100 );
-		DecaySlider->setSliderStyle ( Slider::LinearVertical );
-		DecaySlider->setTextBoxStyle ( Slider::TextBoxBelow, true, 50, 20 );
+    ~AmpFilterModule ();
 
-		addAndMakeVisible ( SustainSlider );
-		SustainSlider->setRange ( 0, 100 );
-		SustainSlider->setSliderStyle ( Slider::LinearVertical );
-		SustainSlider->setTextBoxStyle ( Slider::TextBoxBelow, true, 50, 20 );
-
-		addAndMakeVisible ( ReleaseSlider );
-		ReleaseSlider->setRange ( 0, 100 );
-		ReleaseSlider->setSliderStyle ( Slider::LinearVertical );
-		ReleaseSlider->setTextBoxStyle ( Slider::TextBoxBelow, true, 50, 20 );
-	}
-
-    ~AmpFilterModule ()
-    {
-
-    }
-
-	void paint (Graphics& g) override
-	{
-		g.setColour (BORDER_COLOR);
-		buildModuleBorder (g, CORNERSIZE, THICKNESS, OFFSET);
-
-		// TEMP IDENTIFICATION TEXT
-		g.drawText ("AmpFILTER", 0, 25, getWidth(), getHeight(), Justification::centredTop); 
-	}
-
-	void resized () override
-	{
-		// 
-		Grid grid;
-
-		using Track = Grid::TrackInfo;
-
-		grid.templateRows = { Track (1_fr), Track (10_fr) };
-		grid.templateColumns = { Track (1_fr), Track (1_fr), Track (1_fr), Track (1_fr) };
-
-		grid.items = {
-		    GridItem ( nullptr ),
-		    GridItem ( nullptr ),
-		    GridItem ( nullptr ),
-		    GridItem ( nullptr ),
-		    GridItem ( AttackSlider  ),
-		    GridItem ( DecaySlider   ),
-		    GridItem ( SustainSlider ),
-		    GridItem ( ReleaseSlider )
-		};
-
-		Rectangle <int> bounds = getLocalBounds();
-		bounds = moduleInternalsBounds ( bounds,
-		                                 MODULE_INSIDE_OFFSET,
-		                                 OFFSET,
-		                                 THICKNESS);
-
-		grid.performLayout ( bounds );
-	}
+	void paint (Graphics& g) override;
+	void resized () override;
 
 private:
 	Slider* AttackSlider;
--- Source/EnvFilterModule.h
+++ Source/EnvFilterModule.h
@@ -25,148 +25,20 @@ public:
                       TextButton* lpb,
                       TextButton* bpb,
                       Label*  al, Label*  dl, 
-                      Label*  sl, Label*  rl )                 
-                    : AttackSlider (as), DecaySlider (ds), 
-                      SustainSlider (ss), ReleaseSlider (rs),
-                      AttackSliderLabel (al), DecaySliderLabel (dl),
-                      SustainSliderLabel (sl), ReleaseSliderLabel (rl)
-                      
-    {
-        //
-        addAndMakeVisible ( AttackSlider );
-        AttackSlider->setRange ( 0 , 100 );
-        AttackSlider->setSliderStyle ( Slider::LinearVertical );
-        AttackSlider->setTextBoxStyle ( Slider::TextBoxBelow, true, 50, 20 );
+                      Label*  sl, Label*  rl );
 
-        addAndMakeVisible ( DecaySlider );
-        DecaySlider->setRange ( 0 , 100 );
-        DecaySlider->setSliderStyle ( Slider::LinearVertical );
-        DecaySlider->setTextBoxStyle ( Slider::TextBoxBelow, true, 50, 20 );
-
-        addAndMakeVisible ( SustainSlider );
-        SustainSlider->setRange ( 0 , 100 );
-        SustainSlider->setSliderStyle ( Slider::LinearVertical );
-        SustainSlider->setTextBoxStyle ( Slider::TextBoxBelow, true, 50, 20 );
-
-        addAndMakeVisible ( ReleaseSlider );
-        ReleaseSlider->setRange ( 0 , 100 );
-        ReleaseSlider->setSliderStyle ( Slider::LinearVertical );
-        ReleaseSlider->setTextBoxStyle ( Slider::TextBoxBelow, true, 50, 20 );
-
-        LeftPanel = new LeftSide (ck, rk, hpb, lpb, bpb);
-        addAndMakeVisible ( LeftPanel );
-        
-    }
-
-    ~EnvFilterModule ()
-    {
-        delete LeftPanel;
-    }
-    
-    void paint (Graphics& g) override
-    {
-        g.setColour (BORDER_COLOR); 
-        buildModuleBorder ( g, CORNERSIZE, THICKNESS, OFFSET );
-
-        // TEMP IDENTIFICATION TEXT
-        g.drawText ("EnvFILTER", 0, 25, getWidth(), getHeight(), Justification::centredTop); 
-
-    }
-    
-    void resized () override
-    {
-        //
-        Grid grid;
-        using Track = Grid::TrackInfo;
-
-        grid.templateRows = { 
-            Track (1_fr) , 
-            Track (10_fr) 
-        };
-        
-        grid.templateColumns = { 
-            Track (3_fr), 
-            Track (1_fr), 
-            Track (1_fr), 
-            Track (1_fr), 
-            Track (1_fr)
-        };
-
-        grid.items = {
-            GridItem (nullptr),
-            GridItem (nullptr),
-            GridItem (nullptr),
-            GridItem (nullptr),
-            GridItem (nullptr),
-            GridItem ( LeftPanel ),
-            GridItem (AttackSlider),
-            GridItem (DecaySlider),
-            GridItem (SustainSlider),
-            GridItem (ReleaseSlider)
-        };
-
-        Rectangle <int> bounds = getLocalBounds();
-        bounds = moduleInternalsBounds (bounds, MODULE_INSIDE_OFFSET, OFFSET, THICKNESS);
-
-        grid.performLayout ( bounds );
-    }
+    ~EnvFilterModule ();
+    void paint (Graphics& g) override;
+    void resized () override;
     
     
-    struct LeftSide : public CustomComponent
+    class LeftSide : public CustomComponent
     {
-        LeftSide( Slider* ck, Slider* rk, TextButton* hpb, TextButton* lpb, TextButton* bpb)
-                : CutoffKnob (ck), ResonanceKnob (rk), 
-                  HighPassButton (hpb), LowPassButton (lpb), BandPassButton (bpb)
-        {
-            //
-            addAndMakeVisible ( CutoffKnob );
-            CutoffKnob->setRange ( 0, 100 );
-            CutoffKnob->setSliderStyle ( Slider::Rotary );
-            CutoffKnob->setTextBoxStyle ( Slider::TextBoxBelow, true, 50, 20 );
-
-            addAndMakeVisible ( ResonanceKnob );
-            ResonanceKnob->setRange ( 0, 100 );
-            ResonanceKnob->setSliderStyle ( Slider::Rotary );
-            ResonanceKnob->setTextBoxStyle ( Slider::TextBoxBelow, true, 50, 20 );            
-        }
-
-        void paint (Graphics& g) override
-        {
-        }
-        
-        void resized () override
-        {
-            //
-            Grid grid;
-
-            using Track = Grid::TrackInfo;
-
-            grid.templateRows = { 
-                Track (1_fr) ,
-                Track (1_fr) , 
-                Track (1_fr), 
-                Track (1_fr) , 
-                Track (1_fr)
-            };
-            
-            grid.templateColumns = { 
-                Track (1_fr) 
-            };
-
-            grid.items = {
-                GridItem ( nullptr ),
-                GridItem ( nullptr ),
-                GridItem ( nullptr ),
-                GridItem ( CutoffKnob ),
-                GridItem ( ResonanceKnob )
-            };
-
-            Rectangle <int> bounds = getLocalBounds();
-            bounds = moduleInternalsBounds (bounds, MODULE_INSIDE_OFFSET, OFFSET, THICKNESS);
-
-            grid.performLayout ( bounds );
-        }
-
+    public:
+        LeftSide( Slider* ck, Slider* rk, TextButton* hpb, TextButton* lpb, TextButton* bpb);
+        void paint (Graphics& g) override;
+        void resized () override;
+    private:
         Slider* CutoffKnob;
         Slider* ResonanceKnob;
 

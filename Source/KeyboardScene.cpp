a224d82739389e78ece4c58c26e88d6363c2d7d9
Allen Nobles <allennobles@mac.com>
2018-11-02 13:56:44 -0500

    Refactored onscreen keyboard

--- Source/KeyboardScene.cpp
+++ Source/KeyboardScene.cpp
@@ -12,7 +12,70 @@
 
 KeyboardScene::KeyboardScene ()
 {
+     
+    lower = new Octave();
+    addAndMakeVisible( lower );
 
+    upper = new Octave();
+    addAndMakeVisible( upper );
+}
+
+
+KeyboardScene::~KeyboardScene ()
+{
+    delete lower;
+    delete upper;
+}
+
+void KeyboardScene::paint ( Graphics& g )
+{
+    g.setColour ( BORDER_COLOR );
+}
+
+void KeyboardScene::resized ()
+{
+    Grid grid;
+
+    using Track = Grid::TrackInfo;
+
+    grid.templateRows    = { 
+        Track (1_fr)
+    };
+    
+    grid.templateColumns = { 
+        Track (1_fr), 
+        Track (1_fr)
+    };
+
+    grid.autoColumns = Track (1_fr);
+
+    grid.setGap( 10_px );
+
+
+    grid.items = {
+        GridItem ( lower ),
+        GridItem ( upper )
+    };
+
+    Rectangle <int> bounds = getLocalBounds();
+
+
+    bounds = moduleInternalsBounds ( bounds,
+                                     MODULE_INSIDE_OFFSET,
+                                     OFFSET,
+                                     THICKNESS);
+
+
+    grid.performLayout( bounds );
+}
+
+
+
+
+
+
+KeyboardScene::Octave::Octave()
+{
     c1 = new WhiteKey();
     d1 = new WhiteKey();
     e1 = new WhiteKey();
@@ -26,22 +89,7 @@ KeyboardScene::KeyboardScene ()
     fSharp1 = new BlackKey();
     gSharp1 = new BlackKey();
     aSharp2 = new BlackKey();
-   
-    c2 = new WhiteKey();
-    d2 = new WhiteKey();
-    e2 = new WhiteKey();
-    f2 = new WhiteKey();
-    g2 = new WhiteKey();
-    a3 = new WhiteKey();
-    b3 = new WhiteKey();
-    c3 = new WhiteKey();
-  
-    cSharp2 = new BlackKey();
-    dSharp2 = new BlackKey();
-    fSharp2 = new BlackKey();
-    gSharp2 = new BlackKey();
-    aSharp3 = new BlackKey();
-
+ 
     addAndMakeVisible( c1 );
     addAndMakeVisible( d1 );
     addAndMakeVisible( e1 );
@@ -55,26 +103,11 @@ KeyboardScene::KeyboardScene ()
     addAndMakeVisible( fSharp1 );
     addAndMakeVisible( gSharp1 );
     addAndMakeVisible( aSharp2 );
-
-    addAndMakeVisible( c2 );
-    addAndMakeVisible( d2 );
-    addAndMakeVisible( e2 );
-    addAndMakeVisible( f2 );
-    addAndMakeVisible( g2 );
-    addAndMakeVisible( a3 );
-    addAndMakeVisible( b3 );
-    addAndMakeVisible( c3 );
-
-    addAndMakeVisible( cSharp2 );
-    addAndMakeVisible( dSharp2 );
-    addAndMakeVisible( fSharp2 );
-    addAndMakeVisible( gSharp2 );
-    addAndMakeVisible( aSharp3 );
-
 }
 
-KeyboardScene::~KeyboardScene ()
-{ 
+
+KeyboardScene::Octave::~Octave()
+{
     delete c1;
     delete d1;
     delete e1;
@@ -88,30 +121,14 @@ KeyboardScene::~KeyboardScene ()
     delete fSharp1;
     delete gSharp1;
     delete aSharp2;
-
-    delete c2;
-    delete d2;
-    delete e2;
-    delete f2;
-    delete g2;
-    delete a3;
-    delete b3;
-    delete c3;
-
-    delete cSharp2;
-    delete dSharp2;
-    delete fSharp2;
-    delete gSharp2;
-    delete aSharp3;
 }
 
-void KeyboardScene::paint ( Graphics& g )
-{
-    g.setColour ( BORDER_COLOR );
-    g.drawText ( "KEYBOARD", 0, 25, getWidth(), getHeight(), Justification::centredTop );
-}
 
-void KeyboardScene::resized ()
+void KeyboardScene::Octave::paint ( Graphics& g ) {}
+
+
+
+void KeyboardScene::Octave::resized (  ) 
 {
     Grid grid;
 
@@ -131,7 +148,6 @@ void KeyboardScene::resized ()
 
 
     grid.items = {
-
         GridItem ( c1      ).withArea( 1, 1,  1, 4 ),
         GridItem ( cSharp1 ).withArea( 1, 3,  1, 5 ),
         GridItem ( d1      ).withArea( 1, 4,  1, 7 ),
@@ -143,59 +159,29 @@ void KeyboardScene::resized ()
         GridItem ( gSharp1 ).withArea( 1, 15, 1, 17 ),
         GridItem ( a2      ).withArea( 1, 16, 1, 19 ),
         GridItem ( aSharp2 ).withArea( 1, 18, 1, 20 ),
-        GridItem ( b2      ).withArea( 1, 19, 1, 22 ),
- 
-        GridItem ( c2      ).withArea( 1, 22, 1, 25 ),
-        GridItem ( cSharp2 ).withArea( 1, 24, 1, 26 ),
-        GridItem ( d2      ).withArea( 1, 25, 1, 28 ),
-        GridItem ( dSharp2 ).withArea( 1, 27, 1, 29 ),
-        GridItem ( e2      ).withArea( 1, 28, 1, 31 ),
-        GridItem ( f2      ).withArea( 1, 31, 1, 34 ),
-        GridItem ( fSharp2 ).withArea( 1, 33, 1, 35 ),
-        GridItem ( g2      ).withArea( 1, 34, 1, 37 ),
-        GridItem ( gSharp2 ).withArea( 1, 36, 1, 38 ),
-        GridItem ( a3      ).withArea( 1, 37, 1, 40 ),
-        GridItem ( aSharp3 ).withArea( 1, 39, 1, 41 ),
-        GridItem ( b3      ).withArea( 1, 40, 1, 43 ),
-        GridItem ( c3      ).withArea( 1, 43, 1, 46 )
-       
+        GridItem ( b2      ).withArea( 1, 19, 1, 22 )
     };
 
     Rectangle <int> bounds = getLocalBounds();
 
-    // bounds.setWidth( bounds.getWidth() );
-
-    bounds = moduleInternalsBounds ( bounds,
-                                     MODULE_INSIDE_OFFSET,
-                                     OFFSET,
-                                     THICKNESS);
-
-    grid.performLayout( bounds );
+    grid.performLayout ( bounds );
 }
 
 
 
 
-
-
-
-
-
-
-
-
-KeyboardScene::WhiteKey::WhiteKey()
+KeyboardScene::Octave::WhiteKey::WhiteKey()
 {
 }
 
 
-KeyboardScene::WhiteKey::~WhiteKey()
+KeyboardScene::Octave::WhiteKey::~WhiteKey()
 {
 }
 
 
 
-void KeyboardScene::WhiteKey::paint ( Graphics& g )
+void KeyboardScene::Octave::WhiteKey::paint ( Graphics& g )
 {
     g.setColour( Colours::ivory );
 
@@ -204,25 +190,25 @@ void KeyboardScene::WhiteKey::paint ( Graphics& g )
 }
 
 
-void KeyboardScene::WhiteKey::resized ( )
+void KeyboardScene::Octave::WhiteKey::resized ( )
 {
 }
 
 
 
 
-KeyboardScene::BlackKey::BlackKey()
+KeyboardScene::Octave::BlackKey::BlackKey()
 {
 }
 
 
-KeyboardScene::BlackKey::~BlackKey()
+KeyboardScene::Octave::BlackKey::~BlackKey()
 {
 }
 
 
 
-void KeyboardScene::BlackKey::paint ( Graphics& g )
+void KeyboardScene::Octave::BlackKey::paint ( Graphics& g )
 {
     g.setColour( Colours::black );
 
@@ -234,6 +220,6 @@ void KeyboardScene::BlackKey::paint ( Graphics& g )
 }
 
 
-void KeyboardScene::BlackKey::resized ( )
+void KeyboardScene::Octave::BlackKey::resized ( )
 {
 }
--- Source/KeyboardScene.h
+++ Source/KeyboardScene.h
@@ -22,60 +22,62 @@ public:
     void paint ( Graphics& g ) override;
     void resized () override;
 
-    void buildOctave ();
 
-
-    class WhiteKey : public Keys 
+    class Octave : public CustomComponent 
     {
     public:
-        WhiteKey();
-        ~WhiteKey();
+        Octave();
+        ~Octave();
 
-        void paint ( Graphics& g );
+        void paint ( Graphics& g ) override;
         void resized () override;
 
-    private:
-    };
+
+        class WhiteKey : public Keys 
+        {
+        public:
+            WhiteKey();
+            ~WhiteKey();
+
+            void paint ( Graphics& g );
+            void resized () override;
+
+        private:
+        };
  
 
-    class BlackKey : public Keys
-    {
-    public:
-        BlackKey();
-        ~BlackKey();
+        class BlackKey : public Keys
+        {
+        public:
+            BlackKey();
+            ~BlackKey();
 
-        void paint ( Graphics& g );
-        void resized () override;
+            void paint ( Graphics& g );
+            void resized () override;
+
+        private:
+        };
 
     private:
+
+        WhiteKey* c1;
+        BlackKey* cSharp1;
+        WhiteKey* d1;
+        BlackKey* dSharp1;
+        WhiteKey* e1;
+        WhiteKey* f1;
+        BlackKey* fSharp1;
+        WhiteKey* g1;
+        BlackKey* gSharp1;
+        WhiteKey* a2;
+        BlackKey* aSharp2;
+        WhiteKey* b2;
     };
-    
+
+   
 private:
 
-    WhiteKey* c1;
-    BlackKey* cSharp1;
-    WhiteKey* d1;
-    BlackKey* dSharp1;
-    WhiteKey* e1;
-    WhiteKey* f1;
-    BlackKey* fSharp1;
-    WhiteKey* g1;
-    BlackKey* gSharp1;
-    WhiteKey* a2;
-    BlackKey* aSharp2;
-    WhiteKey* b2;
+    Octave* lower;
+    Octave* upper;
 
-    WhiteKey* c2;
-    BlackKey* cSharp2;
-    WhiteKey* d2;
-    BlackKey* dSharp2;
-    WhiteKey* e2;
-    WhiteKey* f2;
-    BlackKey* fSharp2;
-    WhiteKey* g2;
-    BlackKey* gSharp2;
-    WhiteKey* a3;
-    BlackKey* aSharp3;
-    WhiteKey* b3;
-    WhiteKey* c3;
 };
